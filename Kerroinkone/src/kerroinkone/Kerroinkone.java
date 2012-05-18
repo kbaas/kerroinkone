748499cf4e1c9525dd40bd9d7eac089259b45e8e
Kimmo A Baas <kbaas@dhp-50.cs.helsinki.fi>
2012-05-18 15:57:12 +0300

    päivitetty

---
+++ Kerroinkone/src/kerroinkone/Kerroinkone.java
@@ -0,0 +1,9 @@
+
+package kerroinkone;
+
+public class Kerroinkone {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ Kerroinkone/src/kerroinkone/TestiLuokka.java
@@ -0,0 +1,35 @@
+
+package kerroinkone;
+
+public class TestiLuokka {
+    String nimi;
+    double voimakerroin;
+
+    public TestiLuokka(String nimi, double voimakerroin) {
+        this.nimi = nimi;
+        this.voimakerroin = voimakerroin;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public double getVoimakerroin() {
+        return voimakerroin;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public void setVoimakerroin(double voimakerroin) {
+        this.voimakerroin = voimakerroin;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+    
+}
